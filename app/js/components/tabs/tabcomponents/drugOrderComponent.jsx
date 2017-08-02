c8185664e51ef51d2de0b02fe42ad8a18a23beea
wanjikum <wanjikumillicent5@gmail.com>
2017-08-02 17:12:10 +0300

    [chore] restructure the drug order component

--- app/js/components/tabs/tabcomponents/drugOrderComponent.jsx
+++ app/js/components/tabs/tabcomponents/drugOrderComponent.jsx
@@ -1,87 +1,32 @@
 import React, { Component, PropTypes } from 'react';
 import DatePicker from "react-bootstrap-date-picker";
-import Error from '../../common/error';
-import Loader from '../../common/loader';
-import shortid from 'shortid';
 
 class DrugOrderComponent extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      drugs: [],
-      generics: [],
-      reasons: [],
-      loading: true
-    };
-  }
-
-  componentWillMount() {
-    this.props.fetchData('drug')
-            .then((drugs) => {
-              const allDrugGenerics = {};
-              const conceptsGenerics = drugs.results.map((eachDrug) => {
-                return new Promise((resolve, reject) => {
-                  this.props.fetchData(`drug/${eachDrug.uuid}`).then((drugDetails) => {
-                    allDrugGenerics[drugDetails.concept.uuid] = drugDetails.concept.display;
-                    resolve(allDrugGenerics);
-                  });
-                });
-              });
-              Promise.all(conceptsGenerics).then(drugGenerics => {
-                this.setState({
-                  drugs: drugs.results,
-                  generics: drugGenerics[0]
-                });
-              });
-            }).catch((error) => {
-              this.setState({ error: true, message: error.message });
-            });
-    this.props.fetchData('concept?name=REASON%20ORDER%20STOPPED')
-            .then(conceptReason => {
-              conceptReason.results && this.props.fetchData(`concept/${conceptReason.results[0].uuid}`)
-                    .then(conceptDetails => {
-                      this.setState({
-                        reasons: conceptDetails.answers
-                      });
-                    });
-            });
-    this.setState({ loading: false });
-  }
-
-  showGenerics() {
-    const genericsDisplay = [];
-    for(let uuid in this.state.generics) {
-      genericsDisplay.push(<option value={uuid} key={shortid.generate()}>{this.state.generics[uuid]}</option>);
-    }
-    return genericsDisplay;
-  }
-
-  showOptions(selectData) {
-    return selectData.map((eachOption) =>
-        <option value={eachOption.uuid} key={shortid.generate()}>{eachOption.display}</option>
-    );
-  }
 
   render() {
-    if(this.state.loading) {
-      return (
-                <Loader />
-      );
-    } else if(this.state.error) {
-      return(
-        <Error message={this.state.message}/>
-      );
-    } else {
       return (
         <div>
             <h3>Search By Drug Order</h3>
             <h4 className="text-center">Patients taking specific drugs</h4>
             <form className="form-horizontal">
                 <div className="form-group">
+                    <label htmlFor="selectCategory" className="col-sm-2 control-label">Select</label>
+                            <div className="col-sm-4">
+                                <select className="form-control" id="categoryType" name="categoryType">
+                                    <option>Any</option>
+                                    <option>All</option>
+                                    <option>None</option>
+                                </select>
+                            </div>
+                </div>
+                <div className="form-group">
                     <label htmlFor="drug" className="col-sm-2 control-label">Drug(s)</label>
-                    <div className="col-sm-6">
+                    <div className="col-sm-4">
                         <select className="form-control" multiple="multiple" id="drug" name="drug">
-                            {this.showOptions(this.state.drugs)}
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
                         </select>
                     </div>
                 </div>
@@ -112,7 +57,29 @@ class DrugOrderComponent extends Component {
                     </div>
                     <span className="inline-label">days    (optional)</span>
                 </div>
-
+                <div className="form-group">
+                    <div className="col-sm-offset-2 col-sm-6">
+                        <button type="submit" className="btn btn-success">Search</button>
+                        <button type="reset" className="btn btn-default cancelBtn">Reset</button>
+                    </div>
+                </div>
+                <h4 className="text-center">Patients who stopped or changed a drug</h4>
+                <form className="form-horizontal">
+                <div className="form-group">
+                    <label className="col-sm-2 control-label">When?</label>
+                    <div className="col-sm-2">
+                        <span className="inline-label">Within the last:</span>
+                    </div>
+                    <div className="col-sm-2">
+                        <input className="form-control" type="text" name="month" />
+                    </div>
+                    <span className="inline-label">months and :</span>
+                    <div className="col-sm-2">
+                        <input className="form-control" name="days" type="text" />
+                    </div>
+                    <span className="inline-label">days(optional)</span>
+                </div>
+                </form>
                 <div className="form-group">
                     <label className="col-sm-2 control-label">Date Range</label>
                     <div className="col-sm-1">
@@ -135,7 +102,6 @@ class DrugOrderComponent extends Component {
                     </div>
                     <span className="inline-label">(optional)</span>
                 </div>
-
                 <div className="form-group">
                     <div className="col-sm-offset-2 col-sm-6">
                         <button type="submit" className="btn btn-success">Search</button>
@@ -188,21 +154,30 @@ class DrugOrderComponent extends Component {
                     <div className="col-md-4">
                         <p className="text-center">Reason(s) for change</p>
                         <select className="form-control" multiple="multiple" id="drug" name="drug">
-                            {this.showOptions(this.state.reasons)}
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
                         </select>
                     </div>
 
                     <div className="col-md-4">
                         <p className="text-center">Only these drugs</p>
                         <select className="form-control" multiple="multiple" id="drug" name="drug">
-                            {this.showOptions(this.state.drugs)}
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
                         </select>
                     </div>
 
                     <div className="col-md-4">
                         <p className="text-center">Only these generics</p>
                         <select className="form-control" multiple="multiple" id="drug" name="drug">
-                            {this.showGenerics()}
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
                         </select>
                     </div>
                 </div>
@@ -210,18 +185,13 @@ class DrugOrderComponent extends Component {
                         <div className="col-sm-offset-2 col-sm-6">
                         <button type="submit" className="btn btn-success">Search</button>
                         <button type="reset" className="btn btn-default cancelBtn">Reset</button>
-
                     </div>
                 </div>
+
             </form>
         </div>
       );
     }
-  }
 }
-
-DrugOrderComponent.propTypes = {
-  fetchData: PropTypes.func.isRequired
-};
 
 export default DrugOrderComponent;
